bb6800a3392dc786ff4d20e45c87aca0fad88bb7
DmytroChernihiv <not_mail@not_mail.ua>
2016-09-23 22:40:44 +0300

    1

---
+++ Assets/Scripts/RandomGenerateObject.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomGenerateObject : MonoBehaviour
+{
+    public GameObject[] arrayGameObjects;
+
+    Random random = new Random();
+    
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
